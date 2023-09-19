25d87a9c9c308224188b9b5d31709c1db63932c3
Rohit Kanrar <rohitk@iastate.edu>
2023-09-19 00:34:48 -0500

    added csv files for 68 messages

--- formatting.R
+++ formatting.R
@@ -1,5 +1,5 @@
 source("Research/RA/colon-cancer/func.R")
-final_pairs <-readRDS("Research/RA/colon-cancer/pair_assignment_balanced_after_check_second.rds")
+final_pairs <-readRDS("Research/RA/colon-cancer/pair_assignment_68_balanced_after_check_second.rds")
 n <- length(final_pairs)
 n_pair <- nrow(final_pairs[[1]]) / 2
 main_output <- matrix(0, n, 4*n_pair)
@@ -16,4 +16,4 @@ for(i in 1:n){
 }
 
 colnames(main_output) <- col_name
-write.csv(main_output, "Research/RA/colon-cancer/pair_assignments_second_balanced.csv")
+write.csv(main_output, "Research/RA/colon-cancer/pair_assignments_second_68_balanced.csv")
